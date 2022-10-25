32c38f322453cf51bc6e488890d950af1c18948e
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-10-25 15:36:14 +0200

    first commit mileston 0

---
+++ mileston-0/js/app.js
@@ -0,0 +1,12 @@
+/* 
+**MILESTONE 0:**
+Creare l’array di oggetti con le informazioni fornite.
+**MILESTONE 1:**
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+**MILESTONE 2:**
+Stampare le stesse informazioni su DOM sotto forma di stringhe
+**MILESTONE 3:**
+Stampare delle card formattate contenete immagini e testo (La grafica fornita è solo un esempio ma sentitevi liberi di “graficare” come preferite)
+**BONUS:**
+Aggiungere degli aggettivi (più di uno) che identifichino ogni persona e stamparli nella card
+ */
